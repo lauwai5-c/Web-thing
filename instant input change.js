75a313aeef0927a262806232d0921b53c0a50707
lauwai5-c <40617108+lauwai5-c@users.noreply.github.com>
2018-10-13 22:05:45 +0800

    Create instant input change.js

---
+++ instant input change.js
@@ -0,0 +1,12 @@
+jQuery(function(){   // not function name is needed but Jquery is needed
+
+
+
+  $('#TextValue1').keyup(function() {  // id = input field id
+    console.log('.keyup = ' + $(this).val());
+  });
+
+
+
+  
+});
